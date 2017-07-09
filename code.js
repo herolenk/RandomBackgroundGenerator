b286b1cbc84610c6e38bf95745c4d725a761ecb9
Mustafa Katipoğlu <herolenk@hotmail.com>
2017-07-09 05:19:02 +0300

    RandomBackgroundGenerator
    This example was created to learn git basic

---
+++ code.js
@@ -0,0 +1,154 @@
+
+/* ///////////////////////////////////////////////////*/
+/* //////////////////Blog 1 //////////////////////////*/
+/* ///////////////////////////////////////////////////*/
+
+/*This function for randomly Change Box Color */
+function randombackgroundcolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.backgroundColor = "rgb(" + x + "," + y +","+ z +")";   
+}
+/*This function for randomly Change Border Color */
+function randombordercolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.border = "3px solid rgb(" + x + "," + y +","+ z +")";   
+}
+/*Apply Random Background Color Generator function onclick the button */
+    document.getElementById("firstbtn-clr").onclick = function (){randombackgroundcolorgenerator("firstbox")};
+/*Apply Random Border Color Generator function onclick the button */
+    document.getElementById("firstbtn-bord").onclick = function (){randombordercolorgenerator("firstbox")};
+
+/* ///////////////////////////////////////////////////*/
+/* //////////////////Blog 2 //////////////////////////*/
+/* ///////////////////////////////////////////////////*/
+/*This function for randomly Change Box Color */
+function randombackgroundcolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.backgroundColor = "rgb(" + x + "," + y +","+ z +")";   
+}
+/*This function for randomly Change Border Color */
+function randombordercolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.border = "3px solid rgb(" + x + "," + y +","+ z +")";   
+}
+/*Apply Random Background Color Generator function onclick the button */
+    document.getElementById("secondbtn-clr").onclick = function (){randombackgroundcolorgenerator("secondbox")};
+/*Apply Random Border Color Generator function onclick the button */
+    document.getElementById("secondbtn-bord").onclick = function (){randombordercolorgenerator("secondbox")};
+/* ///////////////////////////////////////////////////*/
+/* //////////////////Blog 3 //////////////////////////*/
+/* ///////////////////////////////////////////////////*/
+/*This function for randomly Change Box Color */
+function randombackgroundcolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.backgroundColor = "rgb(" + x + "," + y +","+ z +")";   
+}
+/*This function for randomly Change Border Color */
+function randombordercolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.border = "3px solid rgb(" + x + "," + y +","+ z +")";   
+}
+/*Apply Random Background Color Generator function onclick the button */
+    document.getElementById("thirdbtn-clr").onclick = function (){randombackgroundcolorgenerator("thirdbox")};
+/*Apply Random Border Color Generator function onclick the button */
+    document.getElementById("thirdbtn-bord").onclick = function (){randombordercolorgenerator("thirdbox")};
+/* ///////////////////////////////////////////////////*/
+/* //////////////////Blog 4 //////////////////////////*/
+/* ///////////////////////////////////////////////////*/
+/*This function for randomly Change Box Color */
+function randombackgroundcolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.backgroundColor = "rgb(" + x + "," + y +","+ z +")";   
+}
+/*This function for randomly Change Border Color */
+function randombordercolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.border = "3px solid rgb(" + x + "," + y +","+ z +")";   
+}
+/*Apply Random Background Color Generator function onclick the button */
+    document.getElementById("fourthbtn-clr").onclick = function (){randombackgroundcolorgenerator("fourthbox")};
+/*Apply Random Border Color Generator function onclick the button */
+    document.getElementById("fourthbtn-bord").onclick = function (){randombordercolorgenerator("fourthbox")};
+/* ///////////////////////////////////////////////////*/
+/* //////////////////Blog 5 //////////////////////////*/
+/* ///////////////////////////////////////////////////*/
+/*This function for randomly Change Box Color */
+function randombackgroundcolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.backgroundColor = "rgb(" + x + "," + y +","+ z +")";   
+}
+/*This function for randomly Change Border Color */
+function randombordercolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.border = "3px solid rgb(" + x + "," + y +","+ z +")";   
+}
+/*Apply Random Background Color Generator function onclick the button */
+    document.getElementById("fifthbtn-clr").onclick = function (){randombackgroundcolorgenerator("fifthbox")};
+/*Apply Random Border Color Generator function onclick the button */
+    document.getElementById("fifthbtn-bord").onclick = function (){randombordercolorgenerator("fifthbox")};
+/* ///////////////////////////////////////////////////*/
+/* //////////////////Blog 6 //////////////////////////*/
+/* ///////////////////////////////////////////////////*/
+
+/*This function for randomly Change Box Color */
+function randombackgroundcolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.backgroundColor = "rgb(" + x + "," + y +","+ z +")";   
+}
+/*This function for randomly Change Border Color */
+function randombordercolorgenerator(boxId) {
+
+    var x = Math.round(Math.random() * 255);
+    var y = Math.round(Math.random() * 255);
+    var z = Math.round(Math.random() * 255);
+        
+    document.getElementById(boxId).style.border = "3px solid rgb(" + x + "," + y +","+ z +")";   
+}
+/*Apply Random Background Color Generator function onclick the button */
+    document.getElementById("sixthbtn-clr").onclick = function (){randombackgroundcolorgenerator("sixthbox")};
+/*Apply Random Border Color Generator function onclick the button */
+    document.getElementById("sixthbtn-bord").onclick = function (){randombordercolorgenerator("sixthbox")};
